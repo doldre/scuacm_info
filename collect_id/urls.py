4cb81620776bed0d8d3e4583530e6900fb596904
jibancanyang <1599664856@qq.com>
2016-06-03 17:35:59 +0800

    hellojibancanyang

--- collect_id/urls.py
+++ collect_id/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+     url(r'^HOME', views.index, name='index'),
     url(r'^add_info$', views.add_info, name='add_info'),
     url(r'^delete_info$', views.delete_info, name='delete_info'),
     url(r'^score_detail', views.score_detail, name='score_detail'),
