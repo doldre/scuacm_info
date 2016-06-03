e15c3931aa838f51876cf4549648f690ef81f947
mathon <luoxinchen96@gmail.com>
2016-06-03 18:23:51 +0800

    mathon

--- collect_id/get_rating.py
+++ collect_id/get_rating.py
@@ -1,30 +1,66 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-def get_bc_rating(username):
-    r = requests.get("http://bestcoder.hdu.edu.cn/rating.php?user=" + username)
-    bsObj = BeautifulSoup(r.text,"html.parser")
-    #print(r.text)
+def get_average(points):
+    points = sorted(points,reverse = True)
+    sum_points = 0
     num = 0
-    for item in bsObj.findAll("span",{"class":"bigggger"}):
+    for item in points:
         num += 1
-        if num == 1:
-            continue
-        return item.get_text()
-    return "0"
-def get_cf_rating(username):
-    r = requests.get("http://codeforces.com/profile/"+username)
+        if num > 3:
+            break
+        sum_points += int(item)
+    if len(points) >= 3:
+        average = sum_points / 3
+    else:
+        average = sum_points / num
+    return average
+
+def get_bc_rating(username):
+    r = requests.get("http://bestcoder.hdu.edu.cn/recentContests.php?user=" + username)
     bsObj = BeautifulSoup(r.text,"html.parser")
     #print(r.text)
-    for item in bsObj.findAll("span",{"style":"font-weight:bold;"}):
-        return item.get_text()
-    return "0"
+    points = []
+    num = 0
+    for item in bsObj.findAll("td"):
+        text = item.get_text()
+        #print(text)
+        pattern = re.compile('^[0-9]+$')
+        match = pattern.match(text)
+        if match:
+            #print(text)
+            num += 1
+            if num % 3 == 0:
+                points.append(text)
+    average = get_average(points)
+    return str(average)
+def get_cf_rating(username):
+    r = requests.get("http://codeforces.com/contests/with/"+username)
+    bsObj = BeautifulSoup(r.text,"html.parser")
+    #print(r.text)
+    points = []
+    num = 0
+    for item in bsObj.find("div",{"id":"pageContent"}).findAll("td"):
+        #print(item)
+        text = item.get_text().strip()
+        #print(text)
+        pattern = re.compile('^[0-9]+$')
+        match = pattern.match(text)
+        if match:
+            #print(text)
+            num += 1
+            if num % 4 == 0:
+                points.append(text)
+    #print(points)
+    average = get_average(points)
+    return str(average)
 
 
 if __name__ == "__main__":
     bc_username = input("please input bc_username:")
     cf_username = input("please input cf_username:")
-    bc_rating = get_bc_rating(bc_username)
-    cf_rating = get_cf_rating(cf_username)
+    #bc_rating = get_bc_rating("liujc")
+    #cf_rating = get_cf_rating("liujc")
     print(bc_username + "\'s bc_rating is " + bc_rating)
     print(cf_username + "\'s cf_rating is " + cf_rating)
